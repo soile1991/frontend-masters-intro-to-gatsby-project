17b360d31f66098f15bff39a20aca9299e499ab4
Panos Soilemezis <panos.soilem@gmail.com>
2022-07-01 14:12:28 +0300

    add index and about page

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import { Link } from "gatsby"
+import * as React from "react"
+
+export default function IndexPage() {
+    return (
+        <main>
+            <h1>Hello Panos</h1>
+            <Link to="/about">About us</Link>
+        </main>
+    )
+}
