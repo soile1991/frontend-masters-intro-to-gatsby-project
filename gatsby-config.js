9db43d70355dcbf28c6dc269af00907df187ed5f
Panos Soilemezis <panos.soilem@gmail.com>
2022-07-02 05:35:22 +0300

    Add gatsby config

---
+++ gatsby-config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    siteMetadata: {
+        siteUrl: `https://www.yourdomain.tld`,
+        title: 'Frontend Masters Intro to Gatsby',
+        description: 'Frontend Masters Intro to Gatsby course projects yoy',
+        image: 'https://res.cloudinary.com/jlengstorf/image/upload/v1628127675/frontend-masters/gatsby-intro/share-image.jpg'
+    },
+    plugins: [
+        {
+            resolve: 'gatsby-plugin-google-analytics',
+            options: {
+                "trackingId": "test"
+            }
+        },
+        "gatsby-plugin-image",
+        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                "name": "images",
+                "path": "./src/images/"
+            },
+            __key: "images"
+        }
+    ]
+};
